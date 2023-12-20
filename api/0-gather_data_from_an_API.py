b98ed8722566077463048512d9964dd5ab42e1f1
amaalyy <amal.elzowawi@gmail.com>
2023-12-20 21:40:59 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,20 +7,17 @@ from sys import argv
 if __name__ == '__main__':
     resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
     resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    t, d = 0, 0
+    for i in resp_todos.json():
+        if i['userId'] == int(argv[1]):
+            t = t + 1
+            if i['completed']:
+                d = d + 1
+    for i in resp_users.json():
+        if i['id'] == int(argv[1]):
+            emp = i['name']
+    print(f"Employee {emp} is done with tasks({d}/{t})")
 
-
-t, d = 0, 0
-for i in resp_todos.json():
-    if i['userId'] == int(argv[1]):
-        t = t + 1
-        if i['completed']:
-            d = d + 1
-for i in resp_users.json():
-    if i['id'] == int(argv[1]):
-        emp = i['name']
-
-print(f"Employee {emp} is done with tasks({d}/{t})")
-
-for i in resp_todos.json():
-    if i['userId'] == int(argv[1]):
-        print(f"  {i['title']}")
+    for i in resp_todos.json():
+        if i['userId'] == int(argv[1]):
+            print(f"  {i['title']}")
