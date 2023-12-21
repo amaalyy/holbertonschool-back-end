17f63325f0eb88bc0f574cafd450acf88d398d06
amaalyy <amal.elzowawi@gmail.com>
2023-12-21 17:23:42 +0100

    Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Gathering the needed informations from the API."""
+import csv
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+
+    for i in resp_users.json():
+        if i['id'] == int(argv[1]):
+            emp = i['name']
+    with open(f'{argv[1]}.csv', 'w') as f:
+        for i in resp_todos.json():
+            if i['userId'] == int(argv[1]):
+                csv.writer(f).writerow(f"{argv[1]} {emp} {i['completed']} {i['title']}")
