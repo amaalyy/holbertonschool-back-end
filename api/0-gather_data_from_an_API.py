e8efd81e42bc6e8035a6eb58b8f43421fed7d297
amaalyy <amal.elzowawi@gmail.com>
2023-12-21 14:58:56 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,16 +7,17 @@ from sys import argv
 if __name__ == '__main__':
     resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
     resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
-    t, d = 0, 0
+    todos_count = 0
+    todos_done = 0
     for i in resp_todos.json():
         if i['userId'] == int(argv[1]):
-            t = t + 1
+            todos_count = todos_count + 1
             if i['completed']:
-                d = d + 1
+                todos_done = todos_done + 1
     for i in resp_users.json():
         if i['id'] == int(argv[1]):
             emp = i['name']
-    print(f"Employee {emp} is done with tasks({d}/{t})")
+    print(f"Employee {emp} is done with tasks({todos_done}/{todos_count})")
 
     for i in resp_todos.json():
         if i['userId'] == int(argv[1]):
