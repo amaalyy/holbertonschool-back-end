3f2519c4e128117c2c93d4706d7e56735577e45e
amaalyy <amal.elzowawi@gmail.com>
2023-12-22 19:26:23 +0100

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
     resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
     resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
     json_dic = dict()
-    json_list = list()
-    small_dic = dict()
+
     for i in resp_users.json():
         json_list = []
         for j in resp_todos.json():
@@ -18,7 +17,8 @@ if __name__ == '__main__':
                 small_dic["username"] = i['username']
                 small_dic["completed"] = j['completed']
                 small_dic["task"] = j['title']
-        json_list.append(small_dic)
+                json_list.append(small_dic)
         json_dic[i['id']] = json_list
+
     with open("todo_all_employees.json", 'w') as f:
         json.dump(json_dic, f)
